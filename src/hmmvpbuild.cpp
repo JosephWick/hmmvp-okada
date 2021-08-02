6a935de89a6a8cda7ebbb45939999d03bd7979bc
Joseph Wick <josephwick@outlook.com>
2021-08-02 11:24:27 -0700

    Update hmmvpbuild.cpp

--- src/hmmvpbuild.cpp
+++ src/hmmvpbuild.cpp
@@ -35,12 +35,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
-#include "util/include/Mpi.hpp"
-#include "util/include/KeyValueFile.hpp"
-#include "util/include/Util.hpp"
-#include "util/include/CodeAnalysis.hpp"
-#include "hmmvp/include/Hd.hpp"
-#include "hmmvp/include/Compress.hpp"
+#include "../util/include/Mpi.hpp"
+#include "../util/include/KeyValueFile.hpp"
+#include "../util/include/Util.hpp"
+#include "../util/include/CodeAnalysis.hpp"
+#include "../hmmvp/include/Hd.hpp"
+#include "../hmmvp/include/Compress.hpp"
 #include "Help.hpp"
 using namespace std;
 using namespace hmmvp;
