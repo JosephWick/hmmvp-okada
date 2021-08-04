ac5ca3d6562b9ca228e58319654c6bdad62730e7
Joseph Wick <josephwick@outlook.com>
2021-08-04 13:02:49 -0700

    Update GreensFnOkada.cpp

--- src/GreensFnOkada.cpp
+++ src/GreensFnOkada.cpp
@@ -106,7 +106,7 @@ inline double GreensFnOkada::Eval (UInt i, UInt j) const {
   double uzz;
 
   dc3d_(ph, pAlpha, &obsx, &obsy, &obsz, &srcdepth, pDip, &zL, pL, &zW, pW, pD1, pD2, pD3, &ux, &uy, &uz, &uxx, &uyx, &uzx, &uxy, &uyy, &uzy, &uxz, &uyz, &uzz);
-  return i;
+  return _mu*(uxy + uyx);
 }
 
 void GreensFnOkada::Init(const KeyValueFile* kvf) throw (Exception) {
