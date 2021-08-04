80b6800b35d9e90c2de31e98e2f7a7472ad4d5f8
Joseph Wick <josephwick@outlook.com>
2021-08-03 21:48:46 -0700

    Update GreensFnTest.cpp

--- src/GreensFnTest.cpp
+++ src/GreensFnTest.cpp
@@ -80,7 +80,7 @@ void GreensFnTest::Init (const KeyValueFile* kvf) throw (Exception) {
   _x = *m;
   if (_x.Size(1) != 3) throw Exception("X must be 3xN.");
 
-  if (kvf->GetDouble("order", &d)) _order = (UInt) *d;
+  if (kvf->GetDouble("order", *d)) _order = (UInt) *d;
   printf("order: %d\n", *d);
 
   kvf->GetDouble("delta", _delta);
