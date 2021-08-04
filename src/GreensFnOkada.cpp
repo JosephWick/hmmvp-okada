a02fc4643ea7527d33cdab5f0c7e629c32614d46
Joseph Wick <josephwick@outlook.com>
2021-08-04 14:16:31 -0700

    Update GreensFnOkada.cpp

--- src/GreensFnOkada.cpp
+++ src/GreensFnOkada.cpp
@@ -64,7 +64,7 @@ private:
 inline double GreensFnOkada::Eval (UInt i, UInt j) const {
   //find individual values to the hmatrix here
   // i is the reeiver, j is the source
-  
+
   // first lets find dimensions of mesh
   int meshL = _L/_dz;
   int meshW = _W/_dz;
@@ -74,8 +74,8 @@ inline double GreensFnOkada::Eval (UInt i, UInt j) const {
 
   // getting parameters
   // for observer position
-  double obsx = (i - 0.5) * _dz;
-  double obsy = (j - 0.5) * _dz;
+  double obsx = 0; //bc 1D rn
+  double obsy = (i - 0.5) * _dz;
   double obsz = 0.0;
 
   // for source depth
