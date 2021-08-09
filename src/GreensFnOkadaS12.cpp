69aced8a6f3de5925592b9f3139cb4b8684d0c8f
Joseph Wick <josephwick@outlook.com>
2021-08-09 14:14:32 -0700

    Update GreensFnOkadaS12.cpp

--- src/GreensFnOkadaS12.cpp
+++ src/GreensFnOkadaS12.cpp
@@ -30,11 +30,12 @@ inline double GreensFnOkadaS12::Eval (UInt i, UInt j) const {
   double G = rho*(pow(Vs, 2))/1e6;
 
   // args
-  double x2 = (double)_x(1,i) + 0.5*_dz;
+  double x2 = 0.0 //(double)_x(1,i) + 0.5*_dz;
   double x3 = (double)_x(2,i) - 0.5*_dz;
 
-  double y2 = _x(1,j);
+  double y2 = 0.0 //_x(1,j);
   double y3 = _x(2,j);
+  printf("%f", y3);
   double W = _dz;
 
   double s12 = (G/(2*M_PI))*( -(x3-y3)/(pow((x2-y2),2) + pow((x3-y3),2))
