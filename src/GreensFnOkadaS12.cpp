41d0a187e2b6ff89c0f0206be65cf4d605218930
Joseph Wick <josephwick@outlook.com>
2021-08-16 12:55:50 -0700

    Update GreensFnOkadaS12.cpp

--- src/GreensFnOkadaS12.cpp
+++ src/GreensFnOkadaS12.cpp
@@ -19,15 +19,13 @@ private:
   // size of blocks
   double _dz;
 
+ //rigidity
+ double _G;
+
   double Eval(UInt i, UInt j) const;
 };
 
 inline double GreensFnOkadaS12::Eval (UInt i, UInt j) const {
-
-  // constants
-  double rho = 2670.0;
-  double Vs = 3464.0;
-  double G = rho*(pow(Vs, 2))/1e6;
 
   // args
   double x2 = 0.0; //(double)_x(1,i) + 0.5*_dz;
@@ -38,7 +36,7 @@ inline double GreensFnOkadaS12::Eval (UInt i, UInt j) const {
 
   double W = _dz;
 
-  double s12 = (G/(2*M_PI))*( -(x3-y3)/(pow((x2-y2),2) + pow((x3-y3),2))
+  double s12 = (_G/(2*M_PI))*( -(x3-y3)/(pow((x2-y2),2) + pow((x3-y3),2))
                               +(x3+y3)/(pow((x2-y2),2) + pow((x3+y3),2))
                               +(x3-y3-W)/(pow((x2-y2),2) + pow((x3-y3-W),2))
                               -(x3+y3+W)/(pow((x2-y2),2) + pow((x3+y3+W),2)) );
@@ -56,6 +54,9 @@ void GreensFnOkadaS12::Init(const KeyValueFile* kvf) throw (Exception) {
   kvf->GetDouble("dz", _dz);
   if (_dz <=0) throw Exception("dz must be greater than 0.");
   printf("dz: %f\n", _dz);
+
+  kvf->GetDouble("G", _G);
+  if (_G <=0) throw Exception("G must be greater than 0.");
 
   double t;
   kvf->GetDouble("tol", t);
