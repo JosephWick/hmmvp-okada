d134afc3fe67c0bd11981c4364aaa4944fe2e83e
Joseph Wick <josephwick@outlook.com>
2021-08-17 16:00:31 -0700

    Update GreensFnOkadaS12.cpp

--- src/GreensFnOkadaS12.cpp
+++ src/GreensFnOkadaS12.cpp
@@ -37,8 +37,8 @@ inline double GreensFnOkadaS12::Eval (UInt i, UInt j) const {
   double idz = i*_dz;
   double jdz = j*_dz;
 
-  double x2loc = _L%(idz);
-  double x3loc = _W%(idz);
+  double x2loc = (double)_L%(idz);
+  double x3loc = (double)_W%(idz);
   double x2 = (double)_x(2,x2loc) - 0.5*_dz;
   double x3 = (double)_x(3,x3loc) - 0.5*_dz;
 
