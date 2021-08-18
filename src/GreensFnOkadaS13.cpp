cc3610bafc305747d0ce59f97513af0d7fd71780
Joseph Wick <josephwick@outlook.com>
2021-08-17 18:01:36 -0700

    Update GreensFnOkadaS13.cpp

--- src/GreensFnOkadaS13.cpp
+++ src/GreensFnOkadaS13.cpp
@@ -103,7 +103,7 @@ inline double GreensFnOkadaS13::Eval (UInt i, UInt j) const {
                               +(x2-y2)/(pow((x2-y2),2) + pow((x3+y3+W),2))
                               );
 
-  printf("i: %d, j: %d, s13: %f\n", i, j, s13);
+  printf("x2: %f, x3: %f, y2: %f, y3: %f, W: %f, s: %f\n", x2, x3, y2, y3, _dz, s13);
 
   return s13;
 }
