9e7e51f763d38d5fedd323489f115a3a6002b5af
Joseph Wick <josephwick@outlook.com>
2021-10-06 21:05:42 -0700

    update W/L defns

--- src/GreensFnShear1212.cpp
+++ src/GreensFnShear1212.cpp
@@ -50,8 +50,8 @@ inline double GreensFnShear1212::Eval(UInt i, UInt j) const {
   x2 = (double)_x(2,j) - y2;
   x3 = (double)_x(3,j) - y3;
 
-  L = abs(2.0*(_y(2,j) - _x(2,j)));
-  W = abs(2.0*(_y(3,j) - _x(3,j)));
+  L = abs(2.0*(_y(2,i) - _x(2,i)));
+  W = abs(2.0*(_y(3,i) - _x(3,i)));
 
   D = (double)_x(3,i) + _trans;
 
