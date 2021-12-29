b2f644a28ff409693b23146f14af850af12a7f14
Joseph Wick <josephwick@outlook.com>
2021-12-29 15:06:23 -0800

    Update GreensFnTest.cpp

--- src/GreensFnTest.cpp
+++ src/GreensFnTest.cpp
@@ -3,12 +3,13 @@
 class GreensFnTest : public ImplGreensFn {
 public:
   virtual void Init(const KeyValueFile* kvf) throw (Exception);
-  virtual Hd* ComputeHd (double eta) { return NewHd(_x, _x, NULL, eta); }
+  virtual Hd* ComputeHd (double eta) { return NewHd(_x, _y, NULL, eta); }
   virtual bool Call(const CompressBlockInfo& cbi, const vector<UInt>& rs,
                     const vector<UInt>& cs, double* B) const;
 
 private:
   Matd _x;
+  Matd _y;
   UInt _order;
   double _delta;
 
@@ -44,7 +45,7 @@ inline double GreensFnTest::Eval (UInt i, UInt j) const {
   // find individual values to the hmatrix here
   // i is the receiver, j is the source
 
-  double z = (double)_x(3,i);
+  double z = (double)_y(3,i);
 
   return z;
 }
@@ -52,13 +53,18 @@ inline double GreensFnTest::Eval (UInt i, UInt j) const {
 void GreensFnTest::Init (const KeyValueFile* kvf) throw (Exception) {
   double d;
   const Matd* m;
+  const Matd* n;
   UInt tmp;
   UInt tmp2;
   double d2;
 
-  if (!kvf->GetMatd("A", m)) throw Exception("Missing X.");
+  if (!kvf->GetMatd("X", m)) throw Exception("Missing X.");
   _x = *m;
   if (_x.Size(1) != 3) throw Exception("X must be 3xN.");
+
+  if (!kvf->GetMatd("Y", n)) throw Exception("Missing Y.");
+  _y = *n;
+  if (_y.Size(1) != 3) throw Exception("Y must be 3xN.");
 
   if (kvf->GetDouble("order", d)) _order = (UInt) d;
   printf("order: %d\n", _order);
